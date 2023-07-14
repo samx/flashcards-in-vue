f1650068e834cf69773e294b58a3965723bcb36c
Samson <samsono@gmail.com>
2023-07-14 16:01:55 -0400

    Add Draw card button in admin section

--- pages/admin.vue
+++ pages/admin.vue
@@ -2,7 +2,13 @@
 <div class="columns">
   <div class="column is-one-quarter has-background-success">
     <div class="container">
-        <CreateCardModal></CreateCardModal>
+        <div class="p-3">
+            <button @click="$router.push({path: '/'})" class="is-fullwidth button ">Draw Card</button>
+        </div>
+        <div class="p-3">
+            <button @click="$refs.CreateCardModal.openModal()" class="is-fullwidth button ">Create Card</button>
+        </div>
+        <CreateCardModal ref="CreateCardModal"></CreateCardModal>
         <div class="">            
             <div class="column" v-for="(bin, index) in bins">
                 <Bin :bin="bin" :key="'bin'+index" :id="'bin'+index"></Bin>
@@ -17,7 +23,7 @@
 </template>
 
 <script>
-import CreateCardModal from "../components/Admin/CreateCardModal.vue";
+import CreateCardModal from "../components/Shared/CreateCardModal.vue";
 import { mapState, mapActions } from "vuex";
 import Bin from "../components/Admin/Bin.vue"
 
